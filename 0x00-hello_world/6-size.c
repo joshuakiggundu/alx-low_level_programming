7c78792df82bcab30af812868b4a8f70b2106dd8
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-13 15:48:07 -0700

    9th commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -it prints sizes
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	char a
+	int b
+	long int c
+	long long int d
+	float e
+	printf("size of a char;%lu", size of a char(a))
+	printf("size of an int;%lu", size of an int(b))
+	printf("size of a long int;%lu", size of a long int(c))
+	printf("size of a long long int;%lu", size of a long long int(d))
+	printf("size of a float;%lu", size of a float(e));
+	return (0);
+}
