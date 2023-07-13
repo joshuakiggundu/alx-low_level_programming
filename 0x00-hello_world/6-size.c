276deeb95c84a0470ad7a5de0bb976326b6ca59f
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-13 15:54:30 -0700

    10th commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long int c
 	long long int d
 	float e
-	printf("size of a char;%lu", size of a char(a))
-	printf("size of an int;%lu", size of an int(b))
-	printf("size of a long int;%lu", size of a long int(c))
-	printf("size of a long long int;%lu", size of a long long int(d))
-	printf("size of a float;%lu", size of a float(e));
+	printf("size of a char;%lu", sizeof(a))
+	printf("size of an int;%lu", sizeof(b))
+	printf("size of a long int;%lu", sizeof(c))
+	printf("size of a long long int;%lu", sizeof(d))
+	printf("size of a float;%lu", sizeof(e));
 	return (0);
 }
