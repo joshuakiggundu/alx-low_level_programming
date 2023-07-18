9bc33e9f8f387bc5c469ca867946ebb8f53e05c9
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-18 03:28:27 -0700

    my 6th vommitb

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -print lowercase letters aprt from q en e
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	char lower = 97;
+
+	while (lower <= 122)
+	{
+	if (lower != 101 && 113)
+	putchar(lower);
+	lower++;
+	}
+	putchar ('\n');
+	return (0);
+}
