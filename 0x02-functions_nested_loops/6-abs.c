a4ce5928062bf0aaf07d01e0c750555f6672183c
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-19 13:57:14 -0700

    code

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - a prototype function
+ *
+ * Description: always amkeing moves
+ * @r: its good
+ * Return: (0)
+ *
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r *  -1);
+}
