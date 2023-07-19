858d953782eb7b0544db74aa82040083948bc441
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-19 05:21:18 -0700

    code is good

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i = '0';
 
-	while (i <= 9)
+	while (i <= '9')
 	{
 		putchar(i);
-		if (i <= 8)
+		if (i <= '8')
 		{
 			putchar(',');
 			putchar(' ');
