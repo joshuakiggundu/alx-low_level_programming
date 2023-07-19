24a40b9c6e06afec502c4c75adc70ec57e7a8137
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-19 11:06:02 -0700

    code is a blesing

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -13,13 +13,13 @@
 int _islower(int c)
 {
 	char i;
-	int lower = 0;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		if (i == c)
 		{
-		lower = 1;
+		return (0);
 		}
 	}
-}	return (lower);
+	return (1);
+}
