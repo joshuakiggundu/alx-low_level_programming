9bb00bf2d5922b00cf73b4047fdff4c8cf989e82
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-19 12:53:47 -0700

    coding

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,6 +11,8 @@
  */
 int print_sign(int n)
 {
+	int negative = -1;
+	char ch = (char) negative;
 	if (n > 0)
 	{
 		_putchar('+');
@@ -24,6 +26,6 @@ int print_sign(int n)
 	else
 	{
 		_putchar('-');
-		return (48);
+		return (ch);
 	}
 }
