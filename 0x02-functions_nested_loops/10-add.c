1c0895b20d1d2e2c556ec371a54af1263c9302d7
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-19 15:50:45 -0700

    looking goof

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - a function to add
+ *
+ * Description: to collect sums
+ * @a: the first integer
+ * @b: the secinf one
+ *
+ * Return: always
+ *
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+
+	return (result);
+}
