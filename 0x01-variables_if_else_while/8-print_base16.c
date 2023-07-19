58150a97ad50f8ca69b9bf8189e479215380af49
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-19 03:43:53 -0700

    coding is fun"

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -prints all the numbers in bsesixteen
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	char i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	char b = 97;
+
+	while (b <= 102)
+	{
+		putchar(b);
+		b++;
+
+	}
+	putchar('\n');
+	return (0);
+}
