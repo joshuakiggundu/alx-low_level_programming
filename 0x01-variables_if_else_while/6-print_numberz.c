fc7a64b1324fa3b4242a9c9b32c05f8f3f6bb155
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-19 02:01:32 -0700

    coding is fun

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -prints all single digit nuumber of bae ten
+ *
+ * Return: always(0)
+ *
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
