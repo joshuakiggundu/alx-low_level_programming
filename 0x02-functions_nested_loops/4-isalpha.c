c35374e82e0a9f4106fe7fe34ed630dcef519b7a
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-19 12:17:13 -0700

    my code

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _isalpha - returns a bunch of numbers
+ *
+ * Description: we are using something nice
+ *
+ * @c: collect the charcter
+ *
+ * Return: Low figures
+ */
+int _isalpha(int c)
+{
+	char lower, upper;
+	int letter = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (lower == c || upper == C)
+			{
+				letter = 1;
+			}
+		}
+	}
+	return (letter);
+}
