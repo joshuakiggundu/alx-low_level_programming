bac94980ef3704eafe816134142cd380c761e6ab
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-21 09:00:38 -0700

    my boys

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,21 +6,18 @@
  *
  */
 void more_numbers(void)
-{
-	int i = 0;
-	int j;
+{	int i,  j;
 
-	while (i <= 10)
+	for (i = 0; i <= 9; i++)
 	{
-		j = 0;
-
-		while (j <= 14)
+		for (j = 0; j <= 14; j++)
 		{
-		_putchar(j);
-		j++;
+			if (j >= 10)
+			{
+			_putchar((j / 10) + '0');
+			}
+		_putchar((j % 10) + '0');
 		}
-		_putchar(i);
-		i++;
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
