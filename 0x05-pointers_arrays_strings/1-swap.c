6ae1f7bc52256d6f4410643071bd1408c943bca5
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-24 09:25:40 -0700

    code is life

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of the pointers
+ *
+ * @a: integer
+ * @b: integer two
+ * Return: none
+ */
+void swap_int(int *a, int *b)
+{
+	int empty;
+	int *c = empty;
+
+	*c = *a;
+	*a = *b;
+	*b = *c;
+}
