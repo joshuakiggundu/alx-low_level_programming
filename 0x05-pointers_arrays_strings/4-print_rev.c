e650888f876cf7f639567f1975b52ea54683714c
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-24 10:02:00 -0700

    trying to copy a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the value in rev
+ *
+ * @s: intger
+ * Return: no value
+ */
+void print_rev(char *s)
+{
+	int a;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	for (a--; a >= 0; a--)
+		_putchar(s[a]);
+		_putchar('\n');
+}
