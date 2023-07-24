3e0a20ea57b58e855cda710ff7f8883c4849e2f9
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-24 10:52:01 -0700

    printing tow figures

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half the function
+ * @str: integer
+ * Return: nothiung
+ *
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int half, i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	half = (len - 1) / 2;
+
+	for (i = (half + 1); i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
