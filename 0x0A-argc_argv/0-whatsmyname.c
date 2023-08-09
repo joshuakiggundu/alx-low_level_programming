c8e87e525584687a3c225062bd097c02ed2d24a8
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-08-09 02:46:44 -0700

    first try in the argc test

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+include "main.h"
+
+/**
+ * main - prints its own name
+ *
+ * @argc: counts the input
+ * @argv: array containimg the strings
+ * Return: always(0)
+ */
+int main(int argc, char *argv[])
+{
+	_putchar ("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+ this file contains the tests about argc and argv
